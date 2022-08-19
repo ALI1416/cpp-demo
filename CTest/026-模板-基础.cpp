d29a7227950c3066e4e9c43ca22e855f7a23a25f
ALI <1416978277@qq.com>
2022-08-19 17:13:25 +0800

    0819

--- CTest/026-模板-基础.cpp
+++ CTest/026-模板-基础.cpp
@@ -1,7 +1,55 @@
 #include<iostream>
 using namespace std;
 
-void main()
+// int交换
+void swapInt(int& a, int& b)
 {
+    int t = a;
+    a = b;
+    b = t;
+}
 
+// double交换
+void swapDouble(double& a, double& b)
+{
+    double t = a;
+    a = b;
+    b = t;
+}
+
+// 声明模板template<typename或class 泛型名称>
+// 一般函数使用typename，类使用class
+// 通用交换
+template<typename T>
+void swapT(T& a, T& b)
+{
+    T t = a;
+    a = b;
+    b = t;
+}
+
+void main026()
+{
+    int a = 123;
+    int b = 456;
+    cout << "a:" << a << ",b:" << b << endl;
+    swapInt(a, b);
+    cout << "交换后：a:" << a << ",b:" << b << endl;
+
+    double c = 1.23;
+    double d = 4.56;
+    cout << "c:" << c << ",d:" << d << endl;
+    swapDouble(c, d);
+    cout << "交换后：c:" << c << ",d:" << d << endl;
+
+    // 使用模板
+    double e = 1.23;
+    double f = 4.56;
+    cout << "e:" << e << ",f:" << f << endl;
+    // 没有标明数据类型，自动推导
+    swapT(e, f);
+    cout << "交换后：e:" << e << ",f:" << f << endl;
+    // 指明数据类型
+    swapT<double>(e, f);
+    cout << "交换后：e:" << e << ",f:" << f << endl;
 }
---
+++ CTest/027-模板-高级.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+
+int addInt(int a, int b)
+{
+    return a + b;
+}
+
+template<typename T>
+T addT(T a, T b)
+{
+    return a + b;
+}
+
+void printAdd(int a, int b)
+{
+    cout << "普通函数printAdd:" << a + b << endl;
+}
+
+template<typename T>
+void printAdd(T a, T b)
+{
+    cout << "函数模板printAdd:" << a + b << endl;
+}
+
+template<typename T>
+void printAdd(T a, T b, T c)
+{
+    cout << "函数模板printAdd:" << a + b + c << endl;
+}
+
+void main()
+{
+    int a = 123;
+    int b = 456;
+    // ASCII:97
+    char c = 'a';
+    // ASCII:98
+    char d = 'b';
+    int e = 789;
+    // 普通函数调用时，会发生类型隐式转换
+    cout << "a+b=" << addInt(a, c) << endl;
+    // 模板不会，需要指定数据类型
+    cout << "a+b=" << addT<int>(a, c) << endl;
+
+    // 如果普通函数与函数模板同名，会默认调用普通函数
+    printAdd(a, b);
+    // 如果要强制调用函数模板，需要加上空参数列表
+    printAdd<>(a, b);
+
+    // 如果函数模板能产生更好的匹配，优先调用函数模板
+    // 如果调用普通函数，需要进行2次隐式转换
+    printAdd(c, d);
+
+    // 模板函数重载
+    printAdd(a, b, e);
+}
